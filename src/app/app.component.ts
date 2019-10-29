d87d5ae5c43b018e59916b2363579683a0a88b7d
Dimitris Mavroudis <im.dimitrismavroudis@gmail.com>
2019-10-29 23:19:38 +0200

    Changed root component name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
-	selector: 'app-root',
+	selector: 'wpasg-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
