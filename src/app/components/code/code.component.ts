3edd713c703ed5bc971ab8e93501799e6d64f91f
Dimitris Mavroudis <im.dimitrismavroudis@gmail.com>
2019-10-29 23:15:34 +0200

    Add copy to clipboard

--- src/app/components/code/code.component.ts
+++ src/app/components/code/code.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, AfterContentChecked, OnInit } from '@angular/core';
+import { Component, Input, OnInit, AfterViewInit } from '@angular/core';
 import hljs from 'highlight.js';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
@@ -8,12 +8,13 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 	templateUrl: './code.component.html',
 	styleUrls: ['./code.component.scss'],
 })
-export class CodeComponent implements OnInit, AfterContentChecked {
+export class CodeComponent implements OnInit, AfterViewInit {
 	textFile: string;
 	link: SafeResourceUrl;
 	@Input() code: string;
 	@Input() type: 'php' | 'css';
 	@Input() download: boolean;
+	@Input() copy: boolean;
 	@Input() downloadFileName: string;
 	@Input() downloadFileType: string;
 
@@ -23,19 +24,16 @@ export class CodeComponent implements OnInit, AfterContentChecked {
 	}
 
 	ngOnInit(): void {
-
+		this.link = this.makeTextFile();
 	}
 
-	ngAfterContentChecked(): void {
-		this.link = this.makeTextFile();
+	ngAfterViewInit(): void {
 		document.querySelectorAll('pre code').forEach((block) => {
 			hljs.highlightBlock(block);
-			console.log('run')
 		});
 	}
 
 	makeTextFile() {
-
 		const data = new Blob([this.code], { type: this.downloadFileType });
 		// If we are replacing a previously generated file we need to
 		// manually revoke the object URL to avoid memory leaks.
@@ -46,6 +44,15 @@ export class CodeComponent implements OnInit, AfterContentChecked {
 		this.textFile = window.URL.createObjectURL(data);
 
 		return this.sanitizer.bypassSecurityTrustResourceUrl(this.textFile);
+	}
+
+	copyToClipborad(str) {
+		const el = document.createElement('textarea');
+		el.value = str;
+		document.body.appendChild(el);
+		el.select();
+		document.execCommand('copy');
+		document.body.removeChild(el);
 	}
 
 }
