af3676599d77d9808a644882d5f313b10175d76e
Dimitris Mavroudis <im.dimitrismavroudis@gmail.com>
2019-10-30 01:48:44 +0200

    Better mockup with css variables

--- src/app/components/wp-admin-mock/wp-admin-mock.component.ts
+++ src/app/components/wp-admin-mock/wp-admin-mock.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Color } from 'src/app/models/colors.model';
+import { DomSanitizer } from '@angular/platform-browser';
 @Component({
 	selector: 'wpasg-mockup',
 	templateUrl: './wp-admin-mock.component.html',
@@ -9,16 +10,27 @@ export class WpAdminMockComponent implements OnInit {
 
 	mockColors: any;
 
-	@Input()colors: Array<Color>;
-	@Input()name: string;
-	@Input()id: string;
+	@Input() colors: Array<Color>;
+	@Input() name: string;
+	@Input() id: string;
 
-	constructor() {
+	constructor(private sanitizer: DomSanitizer) {
 	}
 
 	ngOnInit() {
 
-
 	}
+
+	getCssVariables() {
+		let cssVariables = '';
+		for (const key in this.colors) {
+			if (this.colors[key]) {
+				cssVariables += '--' + key + ':' + this.colors[key] + ';';
+			}
+		}
+		return this.sanitizer.bypassSecurityTrustStyle(cssVariables);
+	}
+
+
 
 }
