7fbbfe3ef59cc56ae7aa8f6a687a202c2e92e925
Dimitris Mavroudis <im.dimitrismavroudis@gmail.com>
2019-11-10 22:13:31 +0200

    Rebrand

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
 	it('should display welcome message', () => {
 		page.navigateTo();
-		expect(page.getTitleText()).toEqual('Welcome to wp-admin-scheme!');
+		expect(page.getTitleText()).toEqual('Welcome to wp-admin-colors!');
 	});
 });
