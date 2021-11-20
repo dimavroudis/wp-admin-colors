bb06fa4b438b5b706deaac844632059a8194dbc3
Dimitris Mavroudis <im.dimitris.mavroudis@gmail.com>
2021-11-20 13:51:44 +0200

    Try new gtag configuartion

--- src/app/services/analytics.service.ts
+++ src/app/services/analytics.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { LoggerService } from './logger.service';
 
 
-declare let gtag: Function;
+declare let dataLayer: Array<any>;
 
 @Injectable({
 	providedIn: 'root'
@@ -13,29 +13,38 @@ export class AnalyticsService {
 	dimensionIndex: number;
 
 	constructor(private logger: LoggerService) {
-		this.gaMeasurmentId = 'UA-79194198-4';
+		this.gaMeasurmentId = 'G-K0JZSKSPDT';
 		this.dimensionIndex = 1;
+
+		this.gtag('js', new Date());
+		this.gtag('config', this.gaMeasurmentId, {
+			'anonymize_ip': true
+		});
+	}
+
+	public gtag(...args: any) {
+		dataLayer.push(args);
 	}
 
 	public setDimension(dimension: string) {
 		const newDimension = {};
 		newDimension['dimenstion' + this.dimensionIndex] = dimension;
 		this.logger.debug('New Dimension', false, newDimension);
-		gtag('config', this.gaMeasurmentId, { 'custom_map': newDimension });
+		this.gtag('config', this.gaMeasurmentId, { 'custom_map': newDimension });
 	}
 
 	public setPageView(path: string) {
 		this.logger.debug('New PageView', false, path);
-		gtag('config', this.gaMeasurmentId,
+		this.gtag('config', this.gaMeasurmentId,
 			{
 				'page_path': path
-				// To Do add page_title [https://developers.google.com/analytics/devguides/collection/gtagjs/pages]
+				// To Do add page_title [https://developers.google.com/analytics/devguides/collection/this.gtagjs/pages]
 			}
 		);
 	}
 
 	public eventEmitter(eventName: string, params: any = {}) {
 		this.logger.debug('New Event ' + eventName, false, params);
-		gtag('event', eventName, params);
+		this.gtag('event', eventName, params);
 	}
 }
